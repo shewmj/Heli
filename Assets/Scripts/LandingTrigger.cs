e67667108b61969a2c2933379e49e461b189ad74
MatthewShew <shewmj@gmail.com>
2019-06-27 16:56:18 -0700

    first commit. Player movement and basic stage setup

---
+++ Assets/Scripts/LandingTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LandingTrigger : MonoBehaviour
+{
+
+    public UnityEvent OnLandEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        if (OnLandEvent == null)
+        {
+            OnLandEvent = new UnityEvent();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical == 0)
+        {
+            OnLandEvent.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+    private Rigidbody2D rb2d;
+    private bool isjump;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        isjump = false;
+
+        
+            
+    }
+    void FixedUpdate()
+    {
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = 0;
+        if (isjump)
+        {
+            vertical += 30;
+            isjump = false;
+        }
+        
+        
+        
+        Vector2 movement = new Vector2(horizontal, vertical);
+        rb2d.AddForce(movement * speed);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isjump = true;
+            animator.SetBool("IsJump", true);
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJump", false);
+    }
+}
